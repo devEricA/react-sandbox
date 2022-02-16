0f6053f85d60935974af9f084d11d87e2a01b89e
devEricA <eric.anderson413@gmail.com>
2022-02-16 14:35:06 -0600

    Create Home, About, LatestNews, and SignUp pages

---
+++ sandbox/src/pages/index.js
@@ -0,0 +1,16 @@
+/*
+    Home page
+*/
+
+
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Eric's React Sandbox</h1>
+        </div>
+    );
+};
+
+export default Home;
